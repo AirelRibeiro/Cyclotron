bad404ad9477b293a0a3bf39436b89e808dedeef
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2023-07-13 18:41:21 -0300

    feat: cria for para aceleração de eletrons

--- cyclotron.py
+++ cyclotron.py
@@ -1,3 +1,9 @@
 def cyclotron(particle, matrix_size):
     matrix = [["1"] * matrix_size for _ in range(matrix_size)]
 
+    if particle == "e":
+        for i in range(matrix_size):
+            if i == 0 or i == matrix_size - 1:
+                matrix[i] = [particle] * matrix_size
+            else:
+                matrix[i][-1] = particle
